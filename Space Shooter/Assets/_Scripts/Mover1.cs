53bddbd6f54703def530781a952e5d7d6d3c161c
KimKim1998 <42846209+KimKim1998@users.noreply.github.com>
2018-10-26 01:29:08 -0400

    k

---
+++ Space Shooter/Assets/_Scripts/Mover1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover1 : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, speed);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
